601e1f473aca812d3244776a410be3797eb64f70
YamanKatmawi <yamankatmawi@gmx.de>
2020-05-05 10:03:18 +0200

    Menu-Movie

---
+++ test/4-5/src/scripts/data.js
@@ -0,0 +1,11 @@
+const recipes = [
+  {
+    id: 1,
+    title: "Midway",
+    ingredients: ["Actor", "produce", "Director"],
+    image: "../img/midway.jpg",
+  },
+];
+
+let data = JSON.stringify(recipes);
+export { data };
